afcef13eb3e9c8af1b112cdd48b8ccc4943649fb
ayoub3bidi <mrayoubabidi@gmail.com>
2021-11-21 15:12:17 +0100

    ✅ Implementing the stories section

--- lib/screens/conversations.dart
+++ lib/screens/conversations.dart
@@ -7,11 +7,32 @@ class Conversations extends StatefulWidget {
 
 TextEditingController _searchController = new TextEditingController();
 
+List storyList = [
+    {
+      "name": "Majdi",
+      "imageUrl": "https://avatars.githubusercontent.com/u/60318289?v=4",
+      "isOnline": true,
+      "hasStory": false,
+    },
+    {
+      "name": "Mohamed",
+      "imageUrl": "https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.6435-1/p320x320/80392624_2641015322650985_962981089951875072_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_ohc=A5C5V0UFhwUAX_hMUMe&tn=YJacikgOMnW4dTk_&_nc_ht=scontent.fnbe1-2.fna&oh=03842eacfe761aee16c35fd9f509cfea&oe=61C0E851",
+      "isOnline": false,
+      "hasStory": true,
+    },
+    {
+      "name": "Rami",
+      "imageUrl": "https://scontent.fnbe1-2.fna.fbcdn.net/v/t39.30808-6/244645230_4379110035515940_642427351699068685_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vniZ-wyVhT8AX-hvvtw&_nc_ht=scontent.fnbe1-2.fna&oh=5f51392ca101f10210b0d434bef9a86e&oe=619E88C0",
+      "isOnline": true,
+      "hasStory": false,
+    }
+  ];
+
 class _ConversationsState extends State<Conversations> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+body: SafeArea(
           child: Container(
         padding: EdgeInsets.only(left: 20, right: 20, top: 15),
         child: ListView(
@@ -57,9 +78,127 @@ class _ConversationsState extends State<Conversations> {
                     border: InputBorder.none),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            _stories(),
           ],
         ),
       )),
     );
   }
 }
+
+_stories() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xFFe9eaec)),
+                  child: Center(
+                    child: Icon(
+                      Icons.add,
+                      size: 33,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: 75,
+                  child: Align(
+                      child: Text(
+                    'Your Story',
+                    overflow: TextOverflow.ellipsis,
+                  )),
+                )
+              ],
+            ),
+          ),
+          Row(
+              children: List.generate(storyList.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    height: 60,
+                    child: Stack(
+                      children: <Widget>[
+                        storyList[index]['hasStory']
+                            ? Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                        color: Colors.blueAccent, width: 3)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(3.0),
+                                  child: Container(
+                                    width: 75,
+                                    height: 75,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                storyList[index]['imageUrl']),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                ),
+                              )
+                            : Container(
+                                width: 70,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            storyList[index]['imageUrl']),
+                                        fit: BoxFit.cover)),
+                              ),
+                        storyList[index]['isOnline']
+                            ? Positioned(
+                                top: 38,
+                                left: 42,
+                                child: Container(
+                                  width: 20,
+                                  height: 20,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF66BB6A),
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                          color: Color(0xFFFFFFFF), width: 3)),
+                                ),
+                              )
+                            : Container()
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 75,
+                    child: Align(
+                        child: Text(
+                      storyList[index]['name'],
+                      overflow: TextOverflow.ellipsis,
+                    )),
+                  )
+                ],
+              ),
+            );
+          }))
+        ],
+      ),
+    );
+  }
